5e1ac38064fcd04583498ad3666b166ff3c07ea7
Nikita Sidorov <nick.sidorov97@gmail.com>
2022-10-31 00:25:43 +0600

    openbrewerydb API tests are done

--- tests/test_brewery.py
+++ tests/test_brewery.py
@@ -1,54 +1,77 @@
-import cerberus
 import requests
-import re
 import pytest
+from jsonschema import validate
 
 base_url = 'https://api.openbrewerydb.org/breweries/'
 brewery_schema = {
-    'type': 'list',
-    'required': True,
-    'schema': {
-        "id": {'type': 'string', 'nullable': True},
-        "name": {'type': 'string', 'nullable': True},
-        "brewery_type": {'type': 'string', 'nullable': True},
-        "street": {'type': 'string', 'nullable': True},
-        "address_2": {'type': 'string', 'nullable': True},
-        "address_3": {'type': 'string', 'nullable': True},
-        "city": {'type': 'string', 'nullable': True},
-        "state": {'type': 'string', 'nullable': True},
-        "county_province": {'type': 'string', 'nullable': True},
-        "postal_code": {'type': 'string', 'nullable': True},
-        "country": {'type': 'string', 'nullable': True},
-        "longitude": {'type': 'string', 'nullable': True},
-        "latitude": {'type': 'string', 'nullable': True},
-        "phone": {'type': 'string', 'nullable': True},
-        "website_url": {'type': 'string', 'nullable': True},
-        "updated_at": {'type': 'string', 'nullable': True},
-        "created_at": {'type': 'string', 'nullable': True}
-    }
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "array",
+    "items": [
+        {
+            "type": "object",
+            "properties": {
+                "id": {
+                    "type": "string"
+                },
+                "name": {
+                    "type": "string"
+                }
+            },
+            "required": [
+                "id",
+                "name"
+            ]
+        }
+    ]
 }
 
 
-@pytest.mark.skip()
 def test_random():
     response = requests.get(base_url + 'random')
     data = response.json()
-    v = cerberus.Validator()
     try:
-        assert v.validate(data, brewery_schema)
-    except TypeError:
-        raise TypeError(data)
+        validate(instance=data, schema=brewery_schema)
+    except AttributeError:
+        raise AttributeError(data)
 
 
 def test_autocomplete():
     response = requests.get(base_url + 'autocomplete?query=dog')
     data = response.json()
-    pattern_id = re.compile(r"(\w|-)*dog(\w|-)*")
-    # pattern_name = re.compile(r"(\w|-)*dog(\w|-)*")
-    matched = 0
-    for result in data:
-        matched += len(pattern_id.findall(result['id']))
     assert response.status_code == 200
-    assert matched == len(data)
+    assert len(data) == 39
 
 
+def test_list_default():
+    response = requests.get(base_url)
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) == 20
+
+
+@pytest.mark.parametrize("test_input, expected", [
+    (1, 1),
+    (2, 2),
+    (49, 49),
+    (50, 50),
+    (51, 50)
+])
+def test_list_per_rage(test_input, expected):
+    response = requests.get(base_url + '?per_page=' + str(test_input))
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) == expected
+
+
+@pytest.mark.parametrize("test_input, expected", [
+    ("san_diego", "San Diego"),
+    ("dallas", "Dallas"),
+    ("new_york", "New York")
+])
+def test_search_by_city(test_input, expected):
+    response = requests.get(base_url + '?by_city=' + str(test_input))
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) <= 20
+    for brewery in data:
+        assert brewery["city"] == expected
